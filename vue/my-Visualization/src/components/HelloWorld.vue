bd42f845319be30e3e99915642cd47c248c6cb0e
lucy <562200219@qq.com>
2025-04-11 23:38:20 +0800

    modified:   vue/my-Visualization/src/components/HelloWorld.vue

--- vue/my-Visualization/src/components/HelloWorld.vue
+++ vue/my-Visualization/src/components/HelloWorld.vue
@@ -13,14 +13,6 @@
       </li>
       <li>
         <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
         >
